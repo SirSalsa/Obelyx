b8094a98344b56a6900ba8723864318d5a30ec9a
Torsten Lareke <torsten.lareke@hotmail.se>
2025-09-14 15:49:08 +0200

    Cleaned up and added basic footer

--- gamebacklog.frontend/src/App.jsx
+++ gamebacklog.frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.scss'
 import Header from './modules/Header/Header'
 import MainTemp from './modules/MainTemp/MainTemp'
+import Footer from './modules/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <MainTemp/>
+      <Footer/>
     </div>
   )
 }
---
+++ gamebacklog.frontend/src/modules/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.scss';
+
+function Footer() {
+  return (
+    <footer>
+      Game Backlog Manager - Self hosted
+    </footer>
+  );
+}
+
+export default Footer;
