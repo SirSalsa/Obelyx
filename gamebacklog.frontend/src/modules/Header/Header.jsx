ed26984b967eee49ca85d743605406c3c23883dc
Torsten Lareke <torsten.lareke@hotmail.se>
2025-09-14 12:37:09 +0200

    Corrected Header

--- gamebacklog.frontend/src/modules/Header/Header.jsx
+++ gamebacklog.frontend/src/modules/Header/Header.jsx
@@ -5,13 +5,18 @@ import "./header.scss"
 function Header(){
     return (
         <header>
-            <img src={logo} alt="Logo"/>
-            <div className="Header_Container">
-                <p>Home</p>
-                <p>Game List</p>
-                <p>Statistics</p>
-                <p>Settings</p>
+            <div className="Header_Wrapper">
+                <div className="Header_Logo">
+                <img src={logo} alt="Logo"/>
+                </div>
+                <div className="Header_Container">
+                    <p>Home</p>
+                    <p>Game List</p>
+                    <p>Statistics</p>
+                    <p>Settings</p>
+                </div>
             </div>
+            
         </header>
     );
 }
