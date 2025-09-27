0849ce99e6c0bfdee22197dc4aefab768e85f628
Torsten Lareke <torsten.lareke@hotmail.se>
2025-09-27 13:50:07 +0200

    Added score overlay to gamecards

--- gamebacklog.frontend/src/modules/GameList/GameCard.jsx
+++ gamebacklog.frontend/src/modules/GameList/GameCard.jsx
@@ -3,7 +3,10 @@ import './GameList.scss';
 function GameCard({ title, imgSrc }) {
   return (
     <div className="GameCard">
-        <img src={imgSrc} alt="Game Cover" />
+        <div className="GameCardImageContainer">
+            <img src={imgSrc} alt="Game Cover" />
+            <div className="GameCardRating">3 ★</div>
+        </div>
         <h3>{title}</h3>
     </div>
   );
